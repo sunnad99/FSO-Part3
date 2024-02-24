6fa8187b59798bd906b7f6001090b293aa9a0662
Sulaiman Nadeem <s.nadeem@prismware.co>
2024-02-24 19:03:52 +0500

    feat: Exercsie 3.3 completed

--- phonebook/index.js
+++ phonebook/index.js
@@ -50,3 +50,14 @@ app.get("/info", (req, res) => {
                ${timeReceived}
             </p> `);
 });
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => p.id === id);
+
+  if (!person) {
+    res.status(404).send(`Person with id ${id} was not found.`);
+  }
+
+  res.json(person);
+});
